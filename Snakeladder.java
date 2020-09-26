067eb17a294b9fb8ae4fb97a1f42e0608bbba156
Rajan-rkg9 <amirajangupta@gmail.com>
2020-09-26 13:37:26 +0530

    uc3 feature added

--- Snakeladder.java
+++ Snakeladder.java
@@ -1,4 +1,5 @@
 public class Snakeladder{
+	public static int position = 0;
 	public static void main(String[] args) {
 		System.out.println("Game starts at position 0");
 		
@@ -8,6 +9,19 @@ public class Snakeladder{
 	{
 		int num =(int)( Math.floor(Math.random() * 10) % 7);
 		return num;
+	}
+		
+	public static void Snakeladderuc3()
+	{
+		int outcome = Snakeladderuc2();
+		int chance =(int)( Math.floor(Math.random() * 10) % 3);
+		
+		/* 0 - not play  1 -ladder 2 - snake */
+		
+		if (check == 1)
+			position = position + chance;
+		else if (check == 2)
+			position = position - chance;
 	}
 	
 }
